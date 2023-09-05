026989bc2bdbb5ab93904287c028efbe413459b2
Akshay Sharma <107939138+AkshayS80@users.noreply.github.com>
2023-09-05 08:33:30 +0530

    Create consonantorvowel.py
    New program

---
+++ Conditionals/consonantorvowel.py
@@ -0,0 +1,7 @@
+#WAP to show if a character is a vowel or a consonant
+
+char=input("Enter a character:")
+if char in 'aeiouAEIOU':
+  print(char,"is a vowel")
+else:
+  print(char,"is a consonant")
